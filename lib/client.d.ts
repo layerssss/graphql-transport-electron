6f2f6c643a769219f7d0d3e3aed448c455bec3c9
Michael Yin <layerssss@gmail.com>
2020-05-29 12:03:17 +1200

    Include built asset

---
+++ lib/client.d.ts
@@ -0,0 +1,23 @@
+import { ApolloLink, Observable, Operation, FetchResult } from 'apollo-link';
+import { ApolloIpcLinkOptions } from './types';
+export declare class IpcLink extends ApolloLink {
+  private ipc;
+  private counter;
+  private channel;
+  private observers;
+  constructor(options: ApolloIpcLinkOptions);
+  request(
+    operation: Operation,
+  ): Observable<
+    FetchResult<
+      {
+        [key: string]: any;
+      },
+      Record<string, any>,
+      Record<string, any>
+    >
+  >;
+  protected listener: (event: any, id: any, type: any, data: any) => void;
+  dispose(): void;
+}
+export declare const createIpcLink: (options: ApolloIpcLinkOptions) => IpcLink;
